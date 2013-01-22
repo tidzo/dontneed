e7e564645a5d6583a57c75826b4b1d48cd7c6b5b
Matt Saunders <matt@tidzo.com>
2013-01-22 08:01:51 +0000

    Added dontneed.c and updated readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ dontneed
 ========
 
 A utility to apply POSIX_FADV_DONTNEED to a specified file.
+
+This was primarily written to workaround a problem with MySQL on Linux Kernels before 2.6.28 where the filesyste cache would fail to honour vm.swappiness=0.  This could cause extreme swap usage with large InnoDB log files.
---
+++ dontneed.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fnctl.h>
+#include <stdlib.h>
+
+/** 
+
+dontneed.c 
+
+applies POSIX_FADV_DONTNEED to the specified file
+
+**/
+
+int main (int argc, char *argv[]) {
+	
+	if (argc != 1) {
+		printf ("Usage: %s filename\n\n", argv[0]);
+		printf ("Applies POSIX_FADV_DONTNEED to the specified file.\n\n");
+		exit(1);
+	}
+	
+	char* path = argv[1];
+	
+	char* mode = "POSIX_FADV_DONTNEED";
+	
+	int flags = O_RDONLY;
+	int fd = open ( path, flags);
+	if ( fd == -1 ) {
+		fprintf (stderr, "Could not open file %s\n", path);
+		return 1;
+	}
+	
+	struct stat fd_stat;
+	
+	if (fstat (fd, &fd_stat ) < 0 ) {
+		fprintf (stderr, "Could not stat file %s\n", path);
+		return 1;
+	}
+	
+	loff_t offset = 0;
+	loff_t length = fd_stat.st_size;
+	
+	int result = posix_fadvise(fd, offset, length, POSIX_FADV_DONTNEED);
+	if (result != ){
+		fprintf(stderr, "Could not set POSIX_FADV_DONTNEED for file %s\n",path);
+	}
+	close(fd);
+	return 0;
+}
